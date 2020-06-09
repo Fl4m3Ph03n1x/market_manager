abc54dfa8a8ea0889504324e361082e9c25a347c
Pedro Martins <palmelapedro@gmail.com>
2020-06-09 12:42:01 +0200

    finished preliminary version of cli client

--- lib/market_manager/cli.ex
+++ lib/market_manager/cli.ex
@@ -1,15 +1,27 @@
 defmodule MarketManager.CLI do
   @moduledoc """
   synopsis:
-    Prints args, possibly multiple times.
+    Manages sell orders in warframe.market.
   usage:
-    $ test10 {options} arg1 arg2 ...
+    $ ./market_manager {options}
+  example:
+    ./market_manager --action=activate syndicates=new_loka,red_veil
   options:
-    --verbose     Add more info.
-    --count=n     Print n times.
+    --action=activate|deactivate        Can be either 'activate' or 'deactivate'.
+                                        Activating a syndicate means placing a sell
+                                        order on warframe.market for each item the
+                                        syndicate has that is in the *products.json*
+                                        file.
+                                        Deactivating a syndicate deletes all orders
+                                        in warframe.market from the given syndicate.
+    --syndicates=syndicate1,syndicate2  Syndicates to be affected by the action.
   """
 
   alias MarketManager
+
+  ##########
+  # Public #
+  ##########
 
   @spec main([any]) :: :ok
   def main([]), do: IO.puts(@moduledoc)
@@ -17,12 +29,13 @@ defmodule MarketManager.CLI do
   def main([help_opt]) when help_opt == "-h", do: IO.puts(@moduledoc)
 
   def main(args) do
-    {opts, positional_args, errors} = parse_args(args)
+    {opts, _positional_args, errors} = parse_args(args)
 
     case errors do
       [] ->
-        process_args(opts, positional_args)
-        show_jason(positional_args)
+        opts
+        |> process_args()
+        |> IO.inspect()
 
       _ ->
         IO.puts("Bad option:")
@@ -31,38 +44,34 @@ defmodule MarketManager.CLI do
     end
   end
 
-  defp show_jason(args) do
-    {:ok, content} = Jason.encode(args)
-    IO.puts("json content: #{content}")
-  end
+  ###########
+  # Private #
+  ###########
 
   defp parse_args(args) do
     {opts, cmd_and_args, errors} =
-      OptionParser.parse(args, strict: [verbose: :boolean, count: :integer])
+      OptionParser.parse(args, strict: [syndicates: :string, action: :string])
 
     {opts, cmd_and_args, errors}
   end
 
-  defp process_args(opts, args) do
-    count = Keyword.get(opts, :count, 1)
+  defp process_args(opts) do
+    syndicates =
+      opts
+      |> Keyword.get(:syndicates)
+      |> String.split(",")
 
-    printfn =
-      if not Keyword.has_key?(opts, :verbose) do
-        fn arg -> IO.puts(arg) end
-      else
-        fn arg ->
-          IO.write("Message: ")
-          IO.puts(arg)
-        end
-      end
+    action = Keyword.get(opts, :action)
 
-    Stream.iterate(0, &(&1 + 1))
-    |> Stream.take(count)
-    |> Enum.each(fn _counter ->
-      Enum.with_index(args)
-      |> Enum.each(fn {arg, idx} ->
-        printfn.("#{idx}. #{arg}")
-      end)
-    end)
+    process_action(action, syndicates)
   end
+
+  defp process_action("activate", syndicates), do:
+    Enum.map(syndicates, &MarketManager.activate/1)
+
+  defp process_action("deactivate", syndicates), do:
+    Enum.map(syndicates, &MarketManager.deactivate/1)
+
+  defp process_action(action, _syndicates), do: {:error, :unknown_action, action}
+
 end
