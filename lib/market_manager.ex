37d3459715eb9f1753e9671d88a1d2c8d0b2ad25
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2020-07-14 15:04:36 +0200

    Dev (#6)
    * improving code readability and cleanup
    
    * removed unused utils library
    
    * Ropping
    
    * refactored interpreter
    
    * regenerated exec
    
    * removed duplicated function
    
    * added test for when writting fails
    
    * adding typespecs
    
    * adding typespecs
    
    * added typespecs

--- lib/market_manager.ex
+++ lib/market_manager.ex
@@ -5,7 +5,36 @@ defmodule MarketManager do
 
   alias MarketManager.Interpreter
 
+  ##########
+  # Types  #
+  ##########
+
+  @type syndicate :: String.t
+  @type error_reason :: atom
+  @type order_id :: String.t
+  @type item_id :: String.t
+
+  #############
+  # Responses #
+  #############
+
+  @type activate_response ::
+    {:ok, :success}
+    | {:partial_success, [{:error, error_reason, item_id}, ...]}
+    | {:error, :unable_to_place_requests, [{:error, error_reason, item_id}]}
+
+  @type deactivate_response ::
+    {:ok, :success}
+    | {:partial_success, [{:error, error_reason, order_id}, ...]}
+    | {:error, :unable_to_delete_orders, [{:error, error_reason, order_id}]}
+
+  #############
+  # Callbacks #
+  #############
+
+  @spec activate(syndicate) :: activate_response
   def activate(syndicate), do: Interpreter.activate(syndicate)
 
+  @spec deactivate(syndicate) :: deactivate_response
   def deactivate(syndicate), do: Interpreter.deactivate(syndicate)
 end
