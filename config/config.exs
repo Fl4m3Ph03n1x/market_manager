8756195f45b1b1c03cba6017bdc45c6a19161e42
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2020-07-27 10:08:48 +0200

    changed config for env variables

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,3 @@
 import Config
 
-config :market_manager,
-  auction_house_cookie: System.fetch_env!("MARKET_MANAGER_WM_COOKIE"),
-  auction_house_token: System.fetch_env!("MARKET_MANAGER_WM_XCSRFTOKEN")
-
 import_config "#{Mix.env()}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -3,4 +3,6 @@ import Config
 config :market_manager,
   products: "test/products.json",
   current_orders: "test/current_orders.json",
-  api_base_url: "http://localhost:8081"
+  api_base_url: "http://localhost:8081",
+  auction_house_cookie: "cookie",
+  auction_house_token: "token"
