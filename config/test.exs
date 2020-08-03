ff0b8adbcc5ca06577232d69abdc9dd28f0782ca
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2020-08-03 10:22:26 +0200

    fixed warframe.market endpoints

--- config/test.exs
+++ config/test.exs
@@ -4,6 +4,6 @@ config :market_manager,
   products: "test/support/products.json",
   current_orders: "test/support/current_orders.json",
   api_base_url: "http://localhost:8082/v1/profile/orders",
-  api_search_url: "http://localhost:8082/items",
+  api_search_url: "http://localhost:8082/v1/items",
   auction_house_cookie: "cookie",
   auction_house_token: "token"
