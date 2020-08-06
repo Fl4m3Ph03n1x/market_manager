622d6a2fdaffd5715fa2caa0806ff243500a03ef
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2020-08-06 10:59:22 +0200

    adding configuration to project

---
+++ apps/auction_house/config/test.exs
@@ -0,0 +1,7 @@
+import Config
+
+config :market_manager,
+  api_base_url: "http://localhost:8082/v1/profile/orders",
+  api_search_url: "http://localhost:8082/v1/items",
+  auction_house_cookie: "cookie",
+  auction_house_token: "token"
