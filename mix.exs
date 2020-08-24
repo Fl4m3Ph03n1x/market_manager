07ea3ea403f78d7cfc9e80541b32490e6cfb750c
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2020-08-24 08:49:40 +0200

    Integrating docs (#28)
    * fixing badge position
    
    * fixing broken pipe
    
    * adding docs
    
    * added resized logo and created a place for logos
    
    * added placee for logos and rebuilt exec
    
    * updated orders
    
    * adding logos
    
    * added Setup section to README
    
    * upated current orders
    
    * removed broken package inch_ex
    
    * changed config for env variables
    
    * adding docs
    
    * documenting public API
    
    * removing broken dependency
    
    * updated docs
    
    * changed documentation to docs folder
    
    * adding price analyst
    
    * finishing price analyst
    
    * Finishing price analyst strategies
    
    * finishing price analyst feature
    
    * removed useless search_name
    
    * Fixing price analyst not working correctly
    
    * fixed warframe.market endpoints
    
    * Making tests work again
    
    * updated orders
    
    * encoding the search and delete urls
    
    * fixing typespecs
    
    * readability refactor
    
    * credo readability refactor
    
    * fixed products file
    
    * new dev version
    
    * added safeguard for too low prices
    
    * fixed case for when external sever is down
    
    * fixing dialyzer specs
    
    * moving to an umbrella app, major refactors
    
    * fixing main umbrella directory
    
    * fixing auction house and umbrella project
    
    * adding mix watch
    
    * adding configuration to project
    
    * refactoring tests, adding integration testing
    
    * refactoring project to confirm with umbrella standards
    
    * added tool-versions
    
    * installed credo
    
    * adding project store, moved tooling to umbrella mix.exs
    
    * added specs
    
    * adde integrations tests for store
    
    * fixing manager
    
    * fixing manager
    
    * converted manager to library
    
    * converted store to library
    
    * fixing cli
    
    * fixing cli
    
    * fixed mix.exs and refactores tests
    
    * generated executable, testing server limit
    
    * refactored for consistency
    
    * lowered concurrent calls to avoid server ddos
    
    * remade executable. Now calls should not cause a ddos
    
    * changed localtion and name of created script
    
    * updted wrong product name
    
    * fixed a product name in red_veil
    
    * added dialyzer to cli and improved error handling
    
    * adding docs and improved test runner
    
    * grammar in docs
    
    * added strategy validation method
    
    * moved strategy verification load to the manager.
    
    * removed old executable, removed test.watch from cli
    
    * added module for shared static settings
    
    * fixing git workflow for building executable
    
    * added dialyxir to project
    
    * created docs

--- mix.exs
+++ mix.exs
@@ -8,7 +8,18 @@ defmodule MarketManager.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: preferred_cli_env()
+      preferred_cli_env: preferred_cli_env(),
+
+      # Docs
+      name: "Market Manager",
+      source_url: "https://github.com/Fl4m3Ph03n1x/market_manager",
+      homepage_url: "https://fl4m3ph03n1x.github.io/market_manager/",
+      docs: [
+        main: "MarketManager", # The main page in the docs
+        logo: "images/resized_logo.png",
+        extras: ["README.md"],
+        output: "docs"
+      ]
     ]
   end
 
@@ -17,7 +28,8 @@ defmodule MarketManager.MixProject do
       {:credo, "~> 1.4", only: [:test, :dev], runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
     ]
   end
 
