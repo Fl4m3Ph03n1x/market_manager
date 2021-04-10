b4f19f5e9ccab309ca5620f90972f4ec6ad47c1a
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2021-04-10 19:13:01 +0200

    adding id and refactoring

--- apps/web_interface/lib/web_interface/commands.ex
+++ apps/web_interface/lib/web_interface/commands.ex
@@ -7,22 +7,24 @@ defmodule WebInterface.Commands do
         description: "
           Activating a syndicate will cause the app to create a sell order on warframe.market for each product of the said syndicate.
           The prices of each item will be determined accoring to a strategy that you can define.
-        "
+        ",
+        id: :activate
       },
       %{
         name: "Deactivate",
         description: "
           Deactivating a syndicate removes all sell orders from waframe.market for the given syndicate.
-        "
+        ",
+        id: :deactivate
       }
     ]
 
 
-  def get_command(name), do:
+  def get_command(id), do:
     list_commands()
-    |> Enum.filter(&by_command_name(&1, name))
+    |> Enum.filter(&by_command_id(&1, id))
     |> hd()
 
-  defp by_command_name(%{name: cname}, name), do: cname == name
+  defp by_command_id(%{id: command_id}, id), do: command_id == id
 
 end
