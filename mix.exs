2d40f88b14a6f7a0612d083646e1f165d83f03ad
Pedro <palmelapedro@gmail.com>
2022-05-18 13:42:36 +0200

    updated version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule MarketManager.MixProject do
   def project,
     do: [
       apps_path: "apps",
-      version: "2.1.5",
+      version: "2.1.2",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
