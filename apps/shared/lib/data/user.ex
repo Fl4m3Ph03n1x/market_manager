fc657e12002e2b900458bbb4f46d96d9483e22ab
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2023-09-18 13:19:37 +0200

    removed comments

--- apps/shared/lib/data/user.ex
+++ apps/shared/lib/data/user.ex
@@ -32,36 +32,3 @@ defmodule Shared.Data.User do
       when is_binary(name) and is_boolean(patreon?),
       do: struct(__MODULE__, user)
 end
-
-# defmodule Shared.Data.User do
-#   @moduledoc """
-#   Represents relevant User information for clients using this AuctionHouse.
-#   """
-
-#   alias Shared.Utils.Structs
-
-#   @enforce_keys [:ingame_name, :patreon?]
-#   defstruct [:ingame_name, :patreon?]
-
-#   @type user ::
-#           %{
-#             (ingame_name :: String.t()) => String.t(),
-#             (patreon? :: String.t()) => boolean()
-#           }
-#           | [ingame_name: String.t(), patreon?: boolean()]
-
-#   @typedoc "User information"
-#   @type t() :: %__MODULE__{
-#           ingame_name: String.t(),
-#           patreon?: boolean()
-#         }
-
-#   @spec new(user()) :: t()
-#   def new(%{"ingame_name" => name, "patreon?" => patreon?} = user)
-#       when is_binary(name) and is_boolean(patreon?),
-#       do: Structs.string_map_to_struct(user, __MODULE__)
-
-#   def new([ingame_name: name, patreon?: patreon?] = user)
-#       when is_binary(name) and is_boolean(patreon?),
-#       do: struct(__MODULE__, user)
-# end
