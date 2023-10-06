761746a0f4437af035f51ba0ab0b28cec4378b43
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2023-10-06 12:08:41 +0200

    added spinning wheel when logging in

--- apps/web_interface/lib/web_interface/live/login_live.ex
+++ apps/web_interface/lib/web_interface/live/login_live.ex
@@ -9,7 +9,7 @@ defmodule WebInterface.LoginLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, logging_in: false) }
   end
 
   @impl true
@@ -20,7 +20,7 @@ defmodule WebInterface.LoginLive do
       |> Manager.login(Map.has_key?(params, "remember-me"))
 
       # show spinning wheel animation
-      {:noreply, socket}
+      {:noreply, assign(socket, logging_in: true)}
   end
 
   @impl true
@@ -29,35 +29,70 @@ defmodule WebInterface.LoginLive do
 
     :ok = UserStore.set_user(user)
 
-    {:noreply,  socket |> redirect(to: ~p"/activate")}
+    socket =
+      socket
+      |> assign(logging_in: false)
+      |> redirect(to: ~p"/activate")
+
+    {:noreply,  socket}
   end
 
   def handle_info({:login, _credentials, {:error, :econnrefused, _data}}, socket) do
-    {:noreply, socket |> put_flash(:error, "Unable to connect to warframe.market. Please verify your internet connection.")}
+    socket =
+      socket
+      |> assign(logging_in: false)
+      |> put_flash(:error, "Unable to connect to warframe.market. Please verify your internet connection.")
+
+    {:noreply, socket}
   end
 
   def handle_info({:login, _credentials, {:error, :wrong_password, _data}}, socket) do
-    {:noreply, socket |> put_flash(:error, "Incorrect Password!")}
+    socket =
+      socket
+      |> assign(logging_in: false)
+      |> put_flash(:error, "Incorrect Password!")
+
+    {:noreply, socket}
   end
 
   def handle_info({:login, _credentials, {:error, :wrong_email, _data}}, socket) do
-    {:noreply, socket |> put_flash(:error, "Your email is incorrect or does not exist!")}
+    socket =
+      socket
+      |> assign(logging_in: false)
+      |> put_flash(:error, "Your email is incorrect or does not exist!")
+
+    {:noreply, socket}
   end
 
   def handle_info({:login, _credentials, {:error, :invalid_email, _data}}, socket) do
-    {:noreply, socket |> put_flash(:error, "Please provide a valid email!")}
+    socket =
+      socket
+      |> assign(logging_in: false)
+      |> put_flash(:error, "Please provide a valid email!")
+
+    {:noreply, socket}
   end
 
   def handle_info({:login, _credentials, {:error, :timeout, _data}}, socket) do
-    {:noreply, socket |> put_flash(:error, "The request timedout, try again later!")}
+    socket =
+      socket
+      |> assign(logging_in: false)
+      |> put_flash(:error, "The request timed out, try again later!")
+
+    {:noreply, socket}
   end
 
   def handle_info({:login, _credentials, {:error, :unknown_error, _data}}, socket) do
-    {:noreply, socket |> put_flash(:error, "An unknown error ocurred, please report it!")}
+    socket =
+      socket
+      |> assign(logging_in: false)
+      |> put_flash(:error, "An unknown error ocurred, please report it!")
+
+    {:noreply, socket}
   end
 
   def handle_info(message, socket) do
-    Logger.error("Unknow message received: #{inspect(message)}")
+    Logger.error("Unknown message received: #{inspect(message)}")
     {:noreply, socket |> put_flash(:error, "Something unexpected happened, please report it!")}
   end
 
