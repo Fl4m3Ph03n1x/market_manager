55fd8e5cb5953900e27e99d4e84af277310de7b9
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2023-10-17 14:33:19 +0200

    fixing tests in umbrella folder

--- config/test.exs
+++ config/test.exs
@@ -9,10 +9,10 @@ config :web_interface, WebInterface.Endpoint,
   check_origin: false
 
 config :store,
-  products: "products.json",
-  current_orders: "current_orders.json",
-  setup: "setup.json",
-  syndicates: "syndicates.json"
+  products: "test_setup/products.json",
+  current_orders: "test_setup/current_orders.json",
+  setup: "test_setup/setup.json",
+  syndicates: "test_setup/syndicates.json"
 
 config :auction_house,
   api_base_url: "http://localhost:8082/v1/profile/orders",
