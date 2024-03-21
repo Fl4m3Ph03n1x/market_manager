7d05518b5f506801bd885cbc56d34ec361ed791e
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2024-03-21 10:50:15 +0100

    version bump

--- apps/web_interface/mix.exs
+++ apps/web_interface/mix.exs
@@ -4,7 +4,7 @@ defmodule WebInterface.MixProject do
   def project do
     [
       app: :web_interface,
-      version: "2.2.1",
+      version: "2.2.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule MarketManager.MixProject do
   def project,
     do: [
       apps_path: "apps",
-      version: "2.1.7",
+      version: "2.1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
