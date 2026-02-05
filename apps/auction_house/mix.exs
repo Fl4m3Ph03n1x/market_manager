68f86f66d28c10efcee5e31237fc952a82e59265
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2026-02-05 12:55:03 +0100

    updated elixir version and app version

--- apps/auction_house/mix.exs
+++ apps/auction_house/mix.exs
@@ -4,12 +4,12 @@ defmodule AuctionHouse.MixProject do
   def project do
     [
       app: :auction_house,
-      version: "4.0.0",
+      version: "5.0.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.18",
+      elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
