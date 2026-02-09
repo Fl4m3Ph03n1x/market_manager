7e1132d59122bfc49a81d5eccd546589b1b78990
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2026-02-09 12:36:01 +0100

    version bump

--- apps/store/mix.exs
+++ apps/store/mix.exs
@@ -4,7 +4,7 @@ defmodule Store.MixProject do
   def project do
     [
       app: :store,
-      version: "5.0.0",
+      version: "5.0.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
