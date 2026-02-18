d2fcd793f705ec0c993e7fd758d7769849d7604d
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2026-02-18 16:20:35 +0100

    added resilience for 502 errors

--- apps/auction_house/test/unit/http_async_client_test.exs
+++ apps/auction_house/test/unit/http_async_client_test.exs
@@ -295,5 +295,66 @@ defmodule AuctionHouse.Impl.HttpAsyncClientTest do
       assert_received(:response_fn_ok)
       assert_received({:login, {:error, :reason}})
     end
+
+    test "returns correct error for 502" do
+      pid = self()
+
+      request = %Request{
+        metadata: %Metadata{send?: false, notify: [pid], operation: :login},
+        args: %{name: "John"}
+      }
+
+      response =
+        {:ok,
+         %HTTPoison.Response{
+           status_code: 502,
+           body: "error code: 502",
+           headers: [
+             {"Date", "Tue, 31 Dec 2024 17:49:43 GMT"},
+             {"Content-Type", "text/plain; charset=UTF-8"},
+             {"Content-Length", "15"},
+             {"Connection", "keep-alive"},
+             {"Report-To",
+              "{\"endpoints\":[{\"url\":\"https:\\/\\/a.nel.cloudflare.com\\/report\\/v4?s=JaQBgCwMziSjmAoxUAaLty6y0rQCmRE2Ho8RgWWIGZNQLL1Z2lSmwOV%2FcRYtiyUyOvaHZatn9en48FMbP%2BEGJAlmxdivK1hEC%2FZQBfP%2FyVp4SBp%2F8d1nQYP%2FXfpijzEDI129D5U%3D\"}],\"group\":\"cf-nel\",\"max_age\":604800}"},
+             {"NEL", "{\"success_fraction\":0,\"report_to\":\"cf-nel\",\"max_age\":604800}"},
+             {"Strict-Transport-Security", "max-age=2592000; includeSubDomains; preload"},
+             {"X-Content-Type-Options", "nosniff"},
+             {"X-Frame-Options", "SAMEORIGIN"},
+             {"Referrer-Policy", "same-origin"},
+             {"Cache-Control", "private, max-age=0, no-store, no-cache, must-revalidate, post-check=0, pre-check=0"},
+             {"Expires", "Thu, 01 Jan 1970 00:00:01 GMT"},
+             {"Server", "cloudflare"},
+             {"CF-RAY", "8fac1b9ca83be060-MAD"},
+             {"server-timing",
+              "cfL4;desc=\"?proto=TCP&rtt=89829&min_rtt=15000&rtt_var=25476&sent=6903&recv=687&lost=0&retrans=248&sent_bytes=9418936&recv_bytes=57802&delivery_rate=359303&cwnd=1120&unsent_bytes=0&cid=b46e4764d9d94e25&ts=161181&x=0\""}
+           ],
+           request_url: "https://api.warframe.market/v1/profile/orders",
+           request: %HTTPoison.Request{
+             method: :post,
+             url: "https://api.warframe.market/v1/profile/orders",
+             headers: [
+               {"x-csrftoken",
+                "##cd456b16cc6fb4ab29717f03f2d3885126253335604d35f9d9372e0dede3731a188083b0c16e036a6d83fb5ead1f2e3c3a58e54a672d625f2243ee5b0b42eb2e"},
+               {"Cookie",
+                "JWT=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzaWQiOiJ1NjBPQUQ1NmhjdjcwazNGczRDcTdlbU96U1JMcEJQOCIsImNzcmZfdG9rZW4iOiI3YTM2YTQwODAwMWQ1NTJjMTYxMjc3MDVjMDQ0MTdiY2ZiMGFkODQ5IiwiZXhwIjoxNzQwODUxMjIyLCJpYXQiOjE3MzU2NjcyMjIsImlzcyI6Imp3dCIsImF1ZCI6Imp3dCIsImF1dGhfdHlwZSI6ImNvb2tpZSIsInNlY3VyZSI6dHJ1ZSwiand0X2lkZW50aXR5IjoielRDOW1WTkMza2xjWUQ3RWtkU0h1VDBYQXFucHJpTFMiLCJsb2dpbl91YSI6ImInaGFja25leS8xLjE3LjEnIiwibG9naW5faXAiOiJiJzk1LjYzLjEzNi4xNjUnIn0.3MkuESHmHNipOKwX0pAvVe9NQuCpoykhP7conHvvRac"},
+               {"Accept", "application/json"},
+               {"Content-Type", "application/json"}
+             ],
+             body:
+               "{\"item_id\":\"54a74455e779892d5e5156aa\",\"order_type\":\"sell\",\"platinum\":19,\"quantity\":1,\"mod_rank\":0}",
+             params: %{},
+             options: []
+           }
+         }}
+
+      response_fn = fn _response ->
+        send(pid, :response_fn_ok)
+        {:ok, nil}
+      end
+
+      assert HttpAsyncClient.handle_response(response, {response_fn, request}) == :ok
+      refute_received(:response_fn_ok)
+      assert_received({:login, {:error, :bad_gateway}})
+    end
   end
 end
