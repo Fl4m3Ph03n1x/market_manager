5b99e56066d2aad9e7ca0f30c73ce58a55164a64
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2026-02-19 14:09:06 +0100

    added tests for new order types

---
+++ apps/shared/test/data/order/ranked_order_test.exs
@@ -0,0 +1,47 @@
+defmodule Shared.Data.Order.RankedOrderTest do
+  @moduledoc false
+
+  use ExUnit.Case
+
+  alias Jason
+  alias Shared.Data.Order.RankedOrder
+
+  test "new/1 returns correct RankedOrder" do
+    data = %{
+      "item_id" => "5962ff05d3ffb64d46e3c47f",
+      "order_type" => "sell",
+      "platinum" => 15,
+      "quantity" => 21,
+      "mod_rank" => 0
+    }
+
+    assert RankedOrder.new(data) == %RankedOrder{
+             item_id: "5962ff05d3ffb64d46e3c47f",
+             order_type: "sell",
+             platinum: 15,
+             quantity: 21,
+             mod_rank: 0
+           }
+  end
+
+  test "converts to JSON correctly" do
+    order = %RankedOrder{
+      item_id: "5962ff05d3ffb64d46e3c47f",
+      order_type: "sell",
+      platinum: 15,
+      quantity: 1,
+      mod_rank: 0
+    }
+
+    json = Jason.encode!(order)
+
+    assert Jason.decode!(json) == %{
+             "type" => "sell",
+             "visible" => true,
+             "platinum" => 15,
+             "quantity" => 1,
+             "itemId" => "5962ff05d3ffb64d46e3c47f",
+             "rank" => 0
+           }
+  end
+end
