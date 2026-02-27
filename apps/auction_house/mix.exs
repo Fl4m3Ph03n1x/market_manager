81840cad5ad74dd35992addfd5b74ab4d750569e
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2026-02-27 09:56:33 +0100

    version bumps

--- apps/auction_house/mix.exs
+++ apps/auction_house/mix.exs
@@ -4,7 +4,7 @@ defmodule AuctionHouse.MixProject do
   def project do
     [
       app: :auction_house,
-      version: "5.0.0",
+      version: "5.1.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
