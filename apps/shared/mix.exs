a4f48d96d858559d79c9fcac8414a264a485040d
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2026-05-27 17:39:20 +0200

    updating elixir versions

--- apps/shared/mix.exs
+++ apps/shared/mix.exs
@@ -4,12 +4,12 @@ defmodule Shared.MixProject do
   def project do
     [
       app: :shared,
-      version: "2.0.0",
+      version: "2.0.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.18",
+      elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls]
--- apps/store/mix.exs
+++ apps/store/mix.exs
@@ -4,12 +4,12 @@ defmodule Store.MixProject do
   def project do
     [
       app: :store,
-      version: "5.0.1",
+      version: "5.0.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.18",
+      elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
