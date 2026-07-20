a34b0ff810dcd4d2be38954ca43f0626d23e65d7
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2026-07-20 11:11:13 +0200

    version bump

--- apps/web_interface/mix.exs
+++ apps/web_interface/mix.exs
@@ -4,7 +4,7 @@ defmodule WebInterface.MixProject do
   def project do
     [
       app: :web_interface,
-      version: "2.3.3",
+      version: "2.3.4",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
